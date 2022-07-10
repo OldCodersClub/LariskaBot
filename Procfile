ecb7b33e74773fa42a737c5aff97170e3f636a62
Aleksey Voko <voko.aleksey@gmail.com>
2022-07-11 01:18:18 +0400

    feat(lariska_bot): add bot webhook

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python -m lariska_bot
--- lariska_bot/bot.py
+++ lariska_bot/bot.py
@@ -1,5 +1,48 @@
+import logging
+import os
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils.executor import start_webhook
+
+
+TOKEN = os.getenv('BOT_TOKEN')
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+
+async def on_startup():
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown():
+    await bot.delete_webhook()
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
 def main():
-    print('Test run message')
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
 
 
 if __name__ == '__main__':
