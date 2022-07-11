fd961553c649e8beebc2a684230e5eeec14754ab
Aleksey Voko <voko.aleksey@gmail.com>
2022-07-11 05:19:46 +0400

    ауфе(lariska_bot/handlers): фвв 'himself_like_this'

--- lariska_bot/handlers/handler.py
+++ lariska_bot/handlers/handler.py
@@ -1,8 +1,9 @@
 from aiogram import types
+from aiogram.dispatcher.filters import Text
 
 from lariska_bot.dispatcher import dp
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+@dp.message_handler(Text(contains=['лариска', 'дура'], ignore_case=True))
+async def himself_like_this(message: types.Message):
+    await message.reply('Сам такой!')
