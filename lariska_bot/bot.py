56d88ff67530b3a6f4f8469e4b1e0c6a68fc3da3
Aleksey Voko <voko.aleksey@gmail.com>
2022-07-12 15:16:43 +0400

    fix(lariska_bot/): fix 'on_shutdown'

--- lariska_bot/bot.py
+++ lariska_bot/bot.py
@@ -13,8 +13,8 @@ async def on_startup(dispatcher):
     await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
 
 
-async def on_shutdown(dispatcher):
-    await bot.delete_webhook()
+# async def on_shutdown(dispatcher):
+#     await bot.delete_webhook()
 
 
 def main():
