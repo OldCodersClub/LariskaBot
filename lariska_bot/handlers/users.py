b778a35ed46164d9e6254053fdad121596c89ffd
Aleksey Voko <voko.aleksey@gmail.com>
2022-09-01 13:27:40 +0400

    feat(lariska_bot/handlers): add 'greetings_Angry_Unicorn'

--- lariska_bot/handlers/users.py
+++ lariska_bot/handlers/users.py
@@ -2,5 +2,6 @@ from lariska_bot.handlers.greetings import *
 
 
 users = {
-    'discodancer75': {'day': 0, 'greetings': greetings_discodancer75}
+    'discodancer75': {'day': 0, 'greetings': greetings_discodancer75},
+    'Angry_Unicorn': {'day': 0, 'greetings': greetings_Angry_Unicorn},
 }
