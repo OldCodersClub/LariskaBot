d2d04d8c5d2e05ece51b766bf7f2c3924d711171
Aleksey Voko <voko.aleksey@gmail.com>
2022-09-01 13:33:39 +0400

    feat(lariska_bot/handlers): add 'Sudmod'

--- lariska_bot/handlers/users.py
+++ lariska_bot/handlers/users.py
@@ -4,4 +4,5 @@ from lariska_bot.handlers.greetings import *
 users = {
     'discodancer75': {'day': 0, 'greetings': greetings_discodancer75},
     'Angry_Unicorn': {'day': 0, 'greetings': greetings_Angry_Unicorn},
+    'Sudmod': {'day': 0, 'greetings': greetings_Sudmod},
 }
