faad746239baa02a98fa2ae52396c2e086ed0ecf
Aleksey Voko <voko.aleksey@gmail.com>
2022-09-01 14:00:55 +0400

    feat(lariska_bot/handlers): add 'user6349'

--- lariska_bot/handlers/users.py
+++ lariska_bot/handlers/users.py
@@ -6,4 +6,5 @@ users = {
     'Angry_Unicorn': {'day': 0, 'greetings': greetings_Angry_Unicorn},
     'Sudmod': {'day': 0, 'greetings': greetings_Sudmod},
     'AlmazovaNatalia': {'day': 0, 'greetings': greetings_AlmazovaNatalia},
+    'user6349': {'day': 0, 'greetings': greetings_user6349},
 }
