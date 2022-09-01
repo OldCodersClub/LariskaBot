f5b43b9475a33fba504b49c664ccf58d751a2fb4
Aleksey Voko <voko.aleksey@gmail.com>
2022-09-01 13:41:15 +0400

    feat(lariska_bot/handlers): add 'AlmazovaNatalia'

--- lariska_bot/handlers/users.py
+++ lariska_bot/handlers/users.py
@@ -5,4 +5,5 @@ users = {
     'discodancer75': {'day': 0, 'greetings': greetings_discodancer75},
     'Angry_Unicorn': {'day': 0, 'greetings': greetings_Angry_Unicorn},
     'Sudmod': {'day': 0, 'greetings': greetings_Sudmod},
+    'AlmazovaNatalia': {'day': 0, 'greetings': greetings_AlmazovaNatalia},
 }
